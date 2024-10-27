2a17f5c76cf738169c94dea98cb06e175839e1bd
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-10-28 00:06:28 +0300

    homepage

---
+++ src/Pages/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <>
+            <div>
+                <div><h1>Hoş Geldiniz.</h1></div>
+                <p>Bu uygulama ile github profillerini arayabilirsiniz.</p>
+            </div>
+        </>
+    )
+}
+
+export default HomePage
