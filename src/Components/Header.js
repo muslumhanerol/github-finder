1a9a769a423795a32f9328f727e8078588f2bdbf
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-11-02 23:42:11 +0300

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import { Container, Navbar } from 'react-bootstrap'
+import { Container, Navbar, NavbarBrand } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
     return (
         <>
             <Navbar bg='primary' variant='dark'>
                 <Container>
-
+                    <NavbarBrand>
+                        <NavLink className="nav-link" to="/">Github Finder App</NavLink>
+                    </NavbarBrand>
                 </Container>
             </Navbar>
         </>
