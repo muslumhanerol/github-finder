7c6637875b74854ba75560fd7dd2fc4bdf660ad4
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-11-02 23:18:22 +0300

    router_ins+

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './Components/Header';
+import HomePage from './Pages/HomePage/HomePage.js';
+import SearchPage from './Pages/SearchPage/SearchPage.js';
 
 function App() {
   return (
     <>
-      <Header />
+      <BrowserRouter>
+        {/* Sayfanın heryerinden görünmesini istediğimiz componentler BrowserRouter içinde tanımlanır. */}
+        <Header />
+
+        {/* Rota tanımlaması yapmak istediğim elementleri Router içine yaz. */}
+        <Routes>
+
+          {/* Rotaların tek tek belirlendiği yer Route. */}
+          <Route path="/" element={<HomePage />} />
+          <Route path='/search' element={<SearchPage />} />
+
+        </Routes>
+
+      </BrowserRouter>
     </>
   );
 }
