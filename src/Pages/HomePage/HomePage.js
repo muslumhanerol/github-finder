21f5239994a68bf8a06e9dee8ceb9cc9d520411e
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-11-05 23:55:08 +0300

    Update HomePage.js

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -5,7 +5,10 @@ const HomePage = () => {
     return (
         <>
             <Container fluid>
-                Merhaba
+                <div>
+                    <h1 className='display-4 text-primary text-center'>Welcome!</h1>
+                    <p className='display-2 text-suscess text-center'>GitHub Finder</p>
+                </div>
             </Container>
         </>
     )
