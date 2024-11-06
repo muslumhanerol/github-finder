b08a3366c6a201a050c0928b526091ce7230b1f6
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-11-06 23:43:26 +0300

    context

--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -1,12 +1,20 @@
 import React from 'react'
-import { Container, Form, InputGroup } from 'react-bootstrap'
+import { Button, Container, Form, InputGroup } from 'react-bootstrap'
 
 const SearchForm = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+    }
     return (
         <>
-            <Container>
-                <Form>
+            <Container className='py-3'>
+                <Form onSubmit={handleSubmit}> {/* Form submit old. çalışacak olan fonk. ismi handleSubmit tir. */}
                     <InputGroup>
+                        <Form.Control placeholder='Enter search github user name.'>
+
+                        </Form.Control>
+                        <Button variant='success' type='submit'>Search</Button>
                     </InputGroup>
                 </Form>
             </Container>
