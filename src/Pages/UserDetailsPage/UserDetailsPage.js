40ff776e36d58ea294744885d4442609e240487a
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-11-14 23:45:31 +0300

    Update UserDetailsPage.js

--- src/Pages/UserDetailsPage/UserDetailsPage.js
+++ src/Pages/UserDetailsPage/UserDetailsPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 
 const UserDetailsPage = () => {
-    console.log(useParams());
+
     return (
         <div>UserDetailsPage</div>
     )
